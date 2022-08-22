74f29a015851cb58f4d525c9bda8fb773ed9c675
Anton Yefanov <antonyefanov@gmail.com>
2022-08-22 11:09:42 +0300

    dark theme done + layout reassignment

--- src/components/Header/Aside/index.tsx
+++ src/components/Header/Aside/index.tsx
@@ -1,5 +1,16 @@
 import { FC } from "react";
 
 export const Aside: FC = () => {
-  return <section className="card aside"></section>;
+  return (
+    <section className="card aside">
+      <img
+        className="aside__image"
+        src={`${process.env.PUBLIC_URL}/assets/hand_heart.png`}
+        alt="thank_you"
+      />
+      <p className="aside__text">
+        Дякую за приділений час. Давайте зробимо щось разом :)
+      </p>
+    </section>
+  );
 };
