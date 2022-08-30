69d0fb4be6a68783323e1309ed98454bd1c85bcb
Anton Yefanov <antonyefanov@gmail.com>
2022-08-30 16:03:14 +0300

    added resume

--- src/components/Footer/CV/index.tsx
+++ src/components/Footer/CV/index.tsx
@@ -1,5 +1,12 @@
 import { FC } from "react";
+import Resume from "../../../documents/Anton_Yefanov.pdf";
 
 export const CV: FC = () => {
-  return <section className="card cv">Резюме</section>;
+  return (
+    <section className="card cv">
+      <a className="cv__link" href={Resume} rel="noreferrer" target="_blank">
+        Резюме
+      </a>
+    </section>
+  );
 };
