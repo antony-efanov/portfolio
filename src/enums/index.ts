2d4b6a93701f277cf75aa7878a1d0559206fff3c
Anton Yefanov <antonyefanov@gmail.com>
2022-09-08 16:49:34 +0300

    changed project

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -2,4 +2,6 @@ export enum Techs {
   React = "React",
   Redux = "Redux",
   SCSS = "SCSS",
+  TypeScript = "TS",
+  Router = "Router",
 }
--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -40,27 +40,43 @@ const MainPage = () => {
           className={"card project projectStore"}
           demoLink="https://antony-efanov.github.io/e-commerce-ua-merch/"
           gitHub="https://github.com/antony-efanov/e-commerce-ua-merch"
-          title="Магазин одягу"
+          title={language === "ua" ? "Магазин одягу" : "Clothes Store"}
           imageSrc={`${process.env.PUBLIC_URL}/assets/projects/merchUA.webp`}
-          description="Реалізував функціонал інтернет-магазину (кошик, улюблене, історія замовлень, тощо). Дані про товари беруться з бекенду (сервіс MockAPI)"
-          techs={[Techs.React, Techs.Redux, Techs.SCSS]}
+          description={
+            language === "ua"
+              ? "Реалізував функціонал інтернет-магазину (кошик, улюблене, історія замовлень, тощо). Дані про товари беруться з бекенду (сервіс MockAPI)"
+              : "Implemented the functionality of the online store (basket, favorites, order history, etc.). Product data is taken from the backend (MockAPI service)"
+          }
+          techs={[Techs.React, Techs.Router, Techs.SCSS]}
         />
         <Project
           className={"card projectLeft projectQR"}
           demoLink="https://antony-efanov.github.io/qr-generator/"
           gitHub="https://github.com/antony-efanov/qr-generator"
-          title="QR Code генератор"
+          title={language === "ua" ? "QR Code генератор" : "QR Code generator"}
           imageSrc={`${process.env.PUBLIC_URL}/assets/projects/QR.webp`}
-          description="Створив простий і швидкий генератор QR-кодів за допомогою бібліотеки «qrcode». Можна змінювати колір та розмір QR-коду"
-          techs={[Techs.React, Techs.Redux, Techs.SCSS]}
+          description={
+            language === "ua"
+              ? "Створив простий і швидкий генератор QR-кодів за допомогою бібліотеки «qrcode». Можна змінювати колір та розмір QR-коду"
+              : "Created a simple and fast QR code generator using the «qrcode» library. You can change the color and size of the QR code"
+          }
+          techs={[Techs.React, Techs.Redux, Techs.TypeScript]}
         />
         <Project
           className={"card project projectRocks"}
-          demoLink="https://antony-efanov.github.io/Crypto-Rocks/"
-          gitHub="https://github.com/antony-efanov/Crypto-Rocks"
-          title="NFT Колекція"
+          demoLink="https://antony-efanov.github.io/reinforced-concrete-products/"
+          gitHub="https://github.com/antony-efanov/reinforced-concrete-products"
+          title={
+            language === "ua"
+              ? "Лендінг для промислової компанії"
+              : "Landing for an industrial company"
+          }
           imageSrc={`${process.env.PUBLIC_URL}/assets/projects/RCP.webp`}
-          description=""
+          description={
+            language === "ua"
+              ? "Зробив лендінг для компанії з виробництва залізобетонних виробів на React / SCSS."
+              : "Made a landing page for a reinforced concrete products company on React / SCSS."
+          }
           techs={[Techs.React, Techs.SCSS]}
         />
         <Contact />
